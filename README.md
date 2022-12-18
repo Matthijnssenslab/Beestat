0ddefb04e1274deba34a40cc5da51f29698a5a08
WardDeb <ward@deboutte.be>
2022-12-18 22:33:10 +0100

    cleanup + pave way for smk & conda

---
+++ README.md
@@ -0,0 +1,1 @@
+# Accompanying repository for association between viral loads and bee colony health in Belgium
---
+++ scripts/metagenomic_heatmap.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+c = pd.read_csv('data/metagenomic_coverage.tsv', sep='\t', index_col=0)
+simpleix = []
+fac = []
+twc = 0
+thc = 0
+for s in list(c.index):
+    if '2013' in s:
+        thc += 1
+        simpleix.append('sample_{}_2013'.format(thc))
+        fac.append('2012')
+    else:
+        twc += 1
+        simpleix.append('sample_{}_2012'.format(twc))
+        fac.append('2013')
+
+g = sns.clustermap(
+    c.T,
+    row_cluster=False,
+    cmap='Blues',
+    vmin=0,
+    col_colors = list(pd.Series(fac).map({'2012': '#ff7f0e', '2013': '#2ca02c'})),
+    xticklabels=False
+)
+g.savefig('output/sup_fig1.png', dpi=300)
